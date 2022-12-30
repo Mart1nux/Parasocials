a0125dcd3990e3fea16fe26d16a1dad9850bed3d
Martinas Mačernius <76516162+Mart1nux@users.noreply.github.com>
2022-12-30 20:23:44 +0200

    Shift implementation (#7)

---
+++ ParasocialsTechnicalDesign/Controllers/ShiftController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParasocialsPOSAPI.Data;
+using ParasocialsPOSAPI.Models;
+
+namespace ParasocialsPOSAPI.Controllers
+{
+    [ApiController]
+    public class ShiftController : Controller
+    {
+        private readonly ParasocialsPOSAPIDbContext dbContext;
+        public ShiftController(ParasocialsPOSAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPut]
+        [Route("/changeShift/{shiftId:Guid}")]
+        public async Task<IActionResult> ChangeShift([FromRoute] Guid shiftId, DateTime startTime, DateTime endTime)
+        {
+            var shift = await dbContext.Shifts.FindAsync(shiftId);
+            if (shift != null)
+            {
+                shift.StartTime = startTime;
+                shift.EndTime = endTime;
+                dbContext.SaveChanges();
+                return Ok(shift);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [Route("/createShift/{startTime}")]
+        public async Task<IActionResult> CreateShift([FromRoute] DateTime startTime, DateTime endTime)
+        {
+            var shift = new Shift()
+            {
+                ShiftId = Guid.NewGuid(),
+                StartTime = startTime,
+                EndTime = endTime
+            };
+            await dbContext.Shifts.AddAsync(shift);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(shift);
+        }
+
+        [HttpGet]
+        [Route("/getShiftById/{shiftId:guid}")]
+        public async Task<IActionResult> GetShift([FromRoute] Guid shiftId)
+        {
+            var shift = await dbContext.Shifts.FindAsync(shiftId);
+            if (shift != null)
+            {
+                return Ok(shift);
+            }
+            return NotFound();
+        }
+
+        [HttpGet]
+        [Route("/getShifts")]
+        public async Task<IActionResult> GetShiftList()
+        {
+            return Ok(await dbContext.Shifts.ToListAsync());
+        }
+    }
+}
