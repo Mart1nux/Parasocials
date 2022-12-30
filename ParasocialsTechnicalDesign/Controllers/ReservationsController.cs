37fabcde83965a778003228c6aeaa8355050c827
GamingGolem <niurkasadainius@gmail.com>
2022-12-30 11:09:53 +0200

    Add reservation controller(not fully working)

---
+++ ParasocialsTechnicalDesign/Controllers/ReservationsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParasocialsPOSAPI.Data;
+using ParasocialsPOSAPI.Models;
+
+namespace ParasocialsPOSAPI.Controllers
+{
+    [ApiController]
+    public class ReservationsController : Controller
+    {
+        private readonly ParasocialsPOSAPIDbContext dbContext;
+
+        public ReservationsController(ParasocialsPOSAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("/reservations")]
+        public async Task<IActionResult> GetReservationsList()
+        {
+            return Ok(await dbContext.Reservations.ToListAsync());
+        }
+
+        [HttpGet]
+        [Route("/reservation/{reservationId:guid}")]
+        public async Task<IActionResult> GetReservation([FromRoute] Guid reservationId)
+        {
+            var reservation = await dbContext.Tips.FindAsync(reservationId);
+            System.Console.WriteLine(reservationId);
+            if (reservation != null)
+            {
+                return Ok(reservation);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [Route("/reservation")]
+        public async Task<IActionResult> CreateReservation(DateTime createdDate, DateTime reservationDate, DateTime duration, string reservationNotes, string location, PremisesType premisetype)
+        {
+            var reservation = new Reservation()
+            {
+                ReservationId = Guid.NewGuid(),
+                CreatedDate = createdDate,
+                ReservationDate = reservationDate,
+                Duration = duration,
+                ReservationNotes = reservationNotes,
+                Premise = new Premise()
+                {
+                    PremiseId = Guid.NewGuid(),
+                    Type = premisetype,
+                    Location = location
+                }
+            };
+            await dbContext.Reservations.AddAsync(reservation);
+            await dbContext.SaveChangesAsync();
+            return Ok(reservation);
+        }
+
+        [HttpPut]
+        [Route("/ReservationUpdateById/{reservationId:guid}")]
+        public async Task<IActionResult> UpdateProductById([FromRoute] Guid reservationId, DateTime createdDate, DateTime reservationDate, DateTime duration, string reservationNotes, string location, PremisesType premisetype)
+        {
+            var reservation = await dbContext.Reservations.FindAsync(reservationId);
+            if (reservation != null)
+            {
+                reservation.CreatedDate = createdDate;
+                reservation.ReservationDate = reservationDate;
+                reservation.Duration = duration;
+                reservation.ReservationNotes = reservationNotes;
+                reservation.Premise.Location = location;
+                reservation.Premise.Type = premisetype;
+                await dbContext.SaveChangesAsync();
+                return Ok(reservation);
+            }
+            return NotFound();
+        }
+
+        [HttpDelete]
+        [Route("/ReservationsDeleteById/{reservationId:guid}")]
+        public async Task<IActionResult> DeleteProductById([FromRoute] Guid reservationId)
+        {
+            var reservation = await dbContext.Reservations.FindAsync(reservationId);
+            if (reservation != null)
+            {
+                dbContext.Reservations.Remove(reservation);
+                await dbContext.SaveChangesAsync();
+                return Ok();
+            }
+            return NotFound();
+        }
+
+    }
+}
+
--- ParasocialsTechnicalDesign/Data/ParasocialsPOSAPIDbContext.cs
+++ ParasocialsTechnicalDesign/Data/ParasocialsPOSAPIDbContext.cs
@@ -12,7 +12,7 @@ namespace ParasocialsPOSAPI.Data
         //public DbSet<Discount> Discounts { get; set; }
         //public DbSet<Order> Orders { get; set; }
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
         //public DbSet<Tax> Taxes { get; set; }
         //public DbSet<Customer> Customers { get; set; }
         //public DbSet<Employee> Employees { get; set; }
