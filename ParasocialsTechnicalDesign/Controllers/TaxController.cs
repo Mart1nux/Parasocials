5be576698434e99e21933a4d45e221d3886d8441
Martinas Mačernius <76516162+Mart1nux@users.noreply.github.com>
2023-01-01 19:08:52 +0200

    Create TaxController.cs

---
+++ ParasocialsTechnicalDesign/Controllers/TaxController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ParasocialsPOSAPI.Data;
+using ParasocialsPOSAPI.Models;
+
+namespace ParasocialsPOSAPI.Controllers
+{
+    [ApiController]
+    public class TaxController : Controller
+    {
+        private readonly ParasocialsPOSAPIDbContext dbContext;
+        public TaxController(ParasocialsPOSAPIDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpPost]
+        [Route("/tax")]
+        public async Task<IActionResult> AddTax([FromRoute] decimal amount, TaxType type, TaxReason reason)
+        {
+            var tax = new Tax()
+            {
+                TaxId = new Guid(),
+                GroupId = groupId,
+                Amount = amount,
+                Reason = reason,
+                Type = type
+            };
+
+            var group = new Group()
+            {
+                GroupId = groupId,
+                GroupName = "Group",
+                DiscountId = new Guid(),
+                
+            };
+
+            tax.GroupId = group.GroupId;
+            tax.Group = group;
+
+            await dbContext.Taxes.AddAsync(tax);
+            await dbContext.SaveChangesAsync();
+
+            return Ok(tax);
+        }
+
+        [HttpPost]
+        [Route("/taxes")]
+        public async Task<IActionResult> GetTaxList()
+        {
+            return Ok(await dbContext.Taxes.Include(e => e.Group).ToListAsync());
+        }
+    }
+}
